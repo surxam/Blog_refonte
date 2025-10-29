60f0bfd31d1b5c9bb21d11641f391ad6a749f27b
surxam <suriam.agesilas@gmail.com>
2025-10-29 10:20:54 -0400

    creation de layout route controller

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    
+    Route::get('/dashboard', [ArticleController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/create', [ArticleController::class, 'create'])->name('dashboard.create');
+    Route::post('/dashboard', [ArticleController::class, 'store'])->name('dashboard.store');
+    Route::get('/dashboard/{id}/edit', [ArticleController::class, 'edit'])->name('dashboard.edit');
+    Route::put('/dashboard/{id}', [ArticleController::class, 'update'])->name('dashboard.update');
+    Route::delete('/dashboard/{id}', [ArticleController::class, 'destroy'])->name('dashboard.destroy');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
